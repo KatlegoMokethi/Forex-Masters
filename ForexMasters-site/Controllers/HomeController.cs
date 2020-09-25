c8632ce48358f5ae920050d628c6ea57fdcb1d84
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-09-25 11:05:07 +0200

    Add error action method & view

--- ForexMasters-site/Controllers/HomeController.cs
+++ ForexMasters-site/Controllers/HomeController.cs
@@ -40,10 +40,9 @@ namespace ForexMasters_site.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
