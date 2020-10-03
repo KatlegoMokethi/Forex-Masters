43028d4011c25efba732f59b843b71e088e03c21
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-10-03 21:34:10 +0200

    Add application Db Context

---
+++ ForexMasters-site/Models/Data/AppDbContext.cs
@@ -0,0 +1,62 @@
+﻿using ForexMasters_site.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace ForexMasters_site.Models.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        DbSet<Video> videos { get; set; }
+        DbSet<Document> documents { get; set; }
+        DbSet<Topic> topics { get; set; }
+        DbSet<Category> categories { get; set; }
+        DbSet<Flashcard> flashcards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Video>().ToTable("Video");
+            modelBuilder.Entity<Document>().ToTable("Document");
+            modelBuilder.Entity<Topic>().ToTable("Topic");
+            modelBuilder.Entity<Category>().ToTable("Category");
+            modelBuilder.Entity<Flashcard>().ToTable("Flashcard");
+            base.OnModelCreating(modelBuilder);
+        }
+        public static async Task CreateAdminAccount(IServiceProvider serviceProvider,
+            IConfiguration configuration)
+        {
+            UserManager<IdentityUser> userManager =
+                serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            RoleManager<IdentityRole> roleManager =
+                serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string name = configuration["Data:AdminUser:Name"];
+            string email = configuration["Data:AdminUser:Email"];
+            string password = configuration["Data:AdminUser:Password"];
+            string role = configuration["Data:AdminUser:Role"];
+
+
+            if (await userManager.FindByNameAsync(name) == null)
+            {
+                if (await roleManager.FindByNameAsync(role) == null)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+                IdentityUser user = new IdentityUser
+                {
+                    UserName = name,
+                    Email = email
+                };
+                IdentityResult result = await userManager.CreateAsync(user, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, role);
+                }
+            }
+        }
+    }
+}
