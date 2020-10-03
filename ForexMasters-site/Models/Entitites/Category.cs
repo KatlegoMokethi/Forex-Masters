596c7b7045701a5604cd77807e161784786051d2
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-10-03 21:33:05 +0200

    Add entities

---
+++ ForexMasters-site/Models/Entitites/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ForexMasters_site.Models.Entities
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryID { get; set; }
+        
+        [Required]
+        [DisplayName("Category")]
+        public string CategoryName { get; set; }
+    }
+}
---
+++ ForexMasters-site/Models/Entitites/Document.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ForexMasters_site.Models.Entities
+{
+    public class Document
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DocumentID { get; set; }
+
+        public string FileName { get; set; }
+        
+        public string FilePath { get; set; }
+
+        public int TopicID { get; set; }
+        public Topic Topic { get; set; }
+    }
+}
