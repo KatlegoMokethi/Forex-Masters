4941a8e986a14fe33141eba27e7fffc8498ca153
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-10-03 21:35:43 +0200

    Update program file

--- ForexMasters-site/Program.cs
+++ ForexMasters-site/Program.cs
@@ -19,6 +19,8 @@ namespace ForexMasters_site
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .UseStartup<Startup>()
+            .UseDefaultServiceProvider(options =>
+            options.ValidateScopes = false);
     }
 }
