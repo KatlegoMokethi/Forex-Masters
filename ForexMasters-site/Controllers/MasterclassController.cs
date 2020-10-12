aea8017ae8b6692f8af96fd983c9e9b9fef66880
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-10-12 17:25:25 +0200

    Add Masterclass  Controller

---
+++ ForexMasters-site/Controllers/MasterclassController.cs
@@ -0,0 +1,25 @@
+﻿using ForexMasters_site.Models.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForexMasters_site.Controllers
+{
+    [Authorize]
+    public class MasterclassController : Controller
+    {
+        private IRepositoryWrapper _repository;
+        public MasterclassController(IRepositoryWrapper repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
