316d6e58f73e662ca64c3e77f0b1800f6b4901f8
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-10-15 22:49:23 +0200

    Added Entities a-fresh

---
+++ ForexMasters-site/Models/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿namespace ForexMasters_site.Models.Entities
+{
+    public class User
+    {
+        public string UserID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+        public string Picture { get; set; }
+        public string Password { get; set; }
+        public decimal Balance { get; set; }
+        public bool isActive { get; set; }
+
+        public User()
+        {
+            this.Balance = 0;
+            this.isActive = false;
+        }
+    }
+}
---
+++ ForexMasters-site/Models/Entities/Video.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ForexMasters_site.Models.Entities
+{
+    public class Video
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int VideoID { get; set; }
+
+        public string VideoName { get; set; }
+        
+        public string VideoPath { get; set; }
+
+        public int TopicID { get; set; }
+        public Topic Topic { get; set; }
+    }
+}
