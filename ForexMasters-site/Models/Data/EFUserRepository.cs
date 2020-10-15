2926dd25a19e8e3f9f485d8769acfff7403ecf51
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-10-15 22:50:32 +0200

    Add User Repository class

---
+++ ForexMasters-site/Models/Data/EFUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using ForexMasters_site.Models.Entities;
+
+namespace ForexMasters_site.Models.Data
+{
+    public class EFUserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public EFUserRepository(AppDbContext appDbContext)
+            : base(appDbContext) { }
+    }
+}
