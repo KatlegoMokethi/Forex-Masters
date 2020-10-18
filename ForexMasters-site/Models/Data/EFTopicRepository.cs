dbd8ffda8526a9795029fd1657f80452b4d7fa49
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-10-18 23:45:07 +0200

    Add Topic Repository EF class

---
+++ ForexMasters-site/Models/Data/EFTopicRepository.cs
@@ -0,0 +1,13 @@
+﻿using ForexMasters_site.Models.Entities;
+
+namespace ForexMasters_site.Models.Data
+{
+    public class EFTopicRepository : RepositoryBase<Topic>, ITopicRepository
+    {
+        public EFTopicRepository(AppDbContext appDbContext)
+            : base(appDbContext)
+        {
+        }
+
+    }
+}
