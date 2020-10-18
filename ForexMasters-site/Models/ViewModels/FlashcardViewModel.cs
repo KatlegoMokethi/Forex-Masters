b0609470cf9a6f0ded90d40a16fd2b7ab8857a0f
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-10-18 17:57:15 +0200

    Add Flashcard View Model

---
+++ ForexMasters-site/Models/ViewModels/FlashcardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ForexMasters_site.Models.ViewModels
+{
+    public class FlashcardViewModel
+    {
+        [Required(ErrorMessage = "Please specify flashcard name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please specify flashcard date")]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "Only .jpg, .jpeg and .png formats are accepted!")]
+        public IFormFile PictureFile { get; set; }
+    }
+}
