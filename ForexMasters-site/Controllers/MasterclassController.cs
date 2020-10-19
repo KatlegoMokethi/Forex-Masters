150877dc1728aa537f773a0226d7c6d928da87a8
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-10-19 23:32:19 +0200

    Update Masterclass Controller

--- ForexMasters-site/Controllers/MasterclassController.cs
+++ ForexMasters-site/Controllers/MasterclassController.cs
@@ -1,4 +1,5 @@
 ﻿using ForexMasters_site.Models.Data;
+using ForexMasters_site.Models.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -20,6 +21,31 @@ namespace ForexMasters_site.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ViewFlashcards()
+        {
+            var Flashcards = _repository.Flashcard.FindAll();
+            return View(Flashcards);
+        }
+
+        public IActionResult Courses()
+        {
+            var Categories = _repository.Category.FindAll();
+            return View(Categories);
+        }
+
+        public IActionResult Topics(int id)
+        {
+            var Topics = _repository.Topic.FindAll().Where(t => t.CategoryID == id);
+            return View(Topics);
+        }
+        public IActionResult TopicDetail(string id)
+        {
+            Topic topic = _repository.Topic.FindByCondition(t => t.TopicID == id).FirstOrDefault();
+            ViewBag.Videos = _repository.Video.FindByCondition(v => v.TopicID == id);
+            ViewBag.Documents = _repository.Document.FindByCondition(d => d.TopicID == id);
+            return View(topic);
         }
     }
 }
