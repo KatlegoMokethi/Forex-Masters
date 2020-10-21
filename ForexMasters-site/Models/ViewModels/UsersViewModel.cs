d9458c972cd1ede2b8550f06c6b5007f58f3273f
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-10-21 12:16:31 +0200

    Add Users View Model

---
+++ ForexMasters-site/Models/ViewModels/UsersViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ForexMasters_site.Models.ViewModels
+{
+    public class UsersViewModel
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Balance { get; set; }
+
+        [DisplayName("Active")]
+        public bool IsActive { get; set; }
+
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
