7949355ad417e7d701ce1536bb24f4703358b589
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-10-23 20:21:18 +0200

    Update model

--- ForexMasters-site/Models/ViewModels/UserViewModel.cs
+++ ForexMasters-site/Models/ViewModels/UserViewModel.cs
@@ -43,7 +43,8 @@ namespace ForexMasters_site.Models.ViewModels
         [FileExtensions(Extensions = "jpg,jpeg,png")]
         public IFormFile PictureFile { get; set; }
 
-        [Required(ErrorMessage = "Enter Password")]
+        [Required]
+        [StringLength(10, ErrorMessage = "The {0} must be at least between {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
