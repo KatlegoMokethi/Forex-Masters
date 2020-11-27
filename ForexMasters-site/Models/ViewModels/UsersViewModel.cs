cbed2347a3b635e176a9ab864663cef8a53091d0
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-11-27 16:44:51 +0200

    Modify Users View Model

--- ForexMasters-site/Models/ViewModels/UsersViewModel.cs
+++ ForexMasters-site/Models/ViewModels/UsersViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -21,6 +22,9 @@ namespace ForexMasters_site.Models.ViewModels
 
         [DisplayName("E-mail")]
         public string Email { get; set; }
+
+        [FileExtensions(Extensions = "jpg,jpeg,png")]
+        public IFormFile PictureFile { get; set; }
 
         [DataType(DataType.Password)]
         public string Password { get; set; }
