6f3473ec7feaf639231331cc3f7615afdcf0e2d4
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-11-30 10:56:02 +0200

    Add controllers

---
+++ ForexMasters-site/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ForexMasters_site.Models;
+
+namespace ForexMasters_site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Signals()
+        {
+            return View();
+        }
+        public IActionResult Resources()
+        {
+            return View();
+        }
+        public IActionResult Payments()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Disclaimer()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
