a5ddf468b65d4cc816102f9caea42af028c2728f
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-11-30 10:56:28 +0200

    Add PDFs View Model

---
+++ ForexMasters-site/Models/ViewModels/PDFViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ForexMasters_site.Models.ViewModels
+{
+    public class PDFViewModel
+    {
+        [Required(ErrorMessage = "Must upload at least one document file!")]
+        [DisplayName("Document(s)")]
+        public List<IFormFile> Documents { get; set; }
+    }
+}
